539e133006150e14a363f8601179ffc4120e0a5c
Faisal Solangi <103946545+Faisalsolangi0@users.noreply.github.com>
2023-05-12 11:50:47 +0500

    Add files via upload

---
+++ site.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./site.css">
+    <title>Site</title>
+</head>
+
+<body>
+    <header>
+        <div class="row">
+            <div class="col-12" style="background-color: darkred;">
+                <h1>Site name</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12" style="background-color: blue;">
+                <span>Link</span>
+                <span>Link</span>
+                <span>Link</span>
+                <span>Link</span>
+                <span>Link</span>
+            </div>
+    </header>
+    </div>
+    <div class="row">
+        <div class="col-9">
+            <h1>Heading</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porroLorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto tempore velit qui fuga
+                iste!
+                Quidem distinctio nihil a unde quibusdam autem, culpa veritatis dignissimos sed quod molestiae et
+                porro
+            </p>
+
+        </div>
+
+        <div class="col-3">
+            <h2>
+                Block header
+            </h2>
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+                <li>item 4</li>
+                <li>item 5</li>
+                <li>item 6</li>
+                <li>item 7</li>
+                <li>item 8</li>
+                <li>item 9</li>
+                <li>item 10</li>
+
+
+            </ul>
+
+        </div>
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-12" id="col12">
+            <h5>
+                &copy; Copyright | Faisal-Solangi 2023
+            </h5>
+        </div>
+    </div>
+
+</body>
+
+</html>
